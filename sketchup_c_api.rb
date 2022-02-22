cf0954552534bdcdf5e5fa4b87ca6f0f136d89f9
Thomas Thomassen <thomas@thomthom.net>
2022-02-22 16:59:05 +0100

    Update sketchup_c_api.rb

--- sketchup_c_api.rb
+++ sketchup_c_api.rb
@@ -4,7 +4,16 @@ require 'fiddle/import'
 module SketchUpAPI
   extend Fiddle::Importer
 
-  sketchup_app = Sketchup.find_support_file('Sketchup.exe')
+  case Sketchup.platform
+  when :platform_osx
+    tools_dir = Sketchup.find_support_file('Tools')
+    sketchup_app = File.expand_path('../../../../Contents/MacOS/SketchUp', tools_dir)
+  when :platform_win
+    sketchup_app = Sketchup.find_support_file('SketchUp.exe')
+  else
+    raise NotImplemented, "#{Sketchup.platform} not supported"
+  end
+
   dlload sketchup_app
 
   extern 'void SUGetAPIVersion(size_t*, size_t*)'
@@ -13,6 +22,6 @@ end
 free = Fiddle::Function.new(Fiddle::RUBY_FREE, [Fiddle::TYPE_VOIDP], Fiddle::TYPE_VOID)
 su_api_version_p = Fiddle::Pointer.malloc(Fiddle::SIZEOF_DOUBLE * 2, free)
 
-SketchUpAPI.SUGetAPIVersion(su_api_version_p, su_api_version_p + 1)
+SketchUpAPI.SUGetAPIVersion(su_api_version_p, su_api_version_p + Fiddle::SIZEOF_SIZE_T)
 su_api_major, su_api_minor = su_api_version_p[0, Fiddle::SIZEOF_SIZE_T * 2].unpack('QQ')
 puts "SUGetAPIVersion: #{su_api_major}.#{su_api_minor}"
